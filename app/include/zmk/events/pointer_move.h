0af57dd83943413569ed003bd7df3225b00c2f1c
harlord <haroldhernandezt@gmail.com>
2025-08-19 19:43:09 -0500

    xmk updates

---
+++ app/include/zmk/events/pointer_move.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
+
+struct zmk_pointer_move {
+    int16_t dx;
+    int16_t dy;
+    int64_t timestamp;
+};
+
+ZMK_EVENT_DECLARE(zmk_pointer_move);
+
+
